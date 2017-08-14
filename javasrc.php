88d80a8249b1353062b457d8b2d6d7f6599e7652
Anıl Yarkın Yücel <a.yarkinyucel@gmail.com>
2017-08-14 13:31:25 +0300

    Update javasrc.php

--- javasrc.php
+++ javasrc.php
@@ -3,6 +3,6 @@
 javasrc.php manages the included javascript files or libraries easily.
 
 -->
-<script type="text/javascript" src="js/jquery/jquery.min.js"></script>
-<script type="text/javascript" src="js/jquery-ui/jquery-ui.min.js"></script>
+<script type="text/javascript" src="jquery/jquery.min.js"></script>
+<script type="text/javascript" src="jquery-ui/jquery-ui.min.js"></script>
 <script type="text/javascript" src="categoryList.js" charset="utf-8"></script>
